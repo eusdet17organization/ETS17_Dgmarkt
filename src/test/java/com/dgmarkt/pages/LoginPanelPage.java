26d59c4108b6cbcd448359557a062e49b382fc4a
anil oral <aniloral3549@gmail.com>
2025-06-24 17:57:08 +0200

    LoginPanelPage class olusturuldu. icerisinde loginPanel methodu ile site korumasi icin paneldeki ilk login islemi gerceklesti.  login.feature icindeki first Login senaryosu icin stepdefinations tamamlandi.

---
+++ src/test/java/com/dgmarkt/pages/LoginPanelPage.java
@@ -0,0 +1,32 @@
+package com.dgmarkt.pages;
+
+import com.dgmarkt.utilities.ConfigurationReader;
+import com.dgmarkt.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPanelPage extends BasePage{
+
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement emailBox;
+
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@name='login']")
+    public WebElement loginButton;
+
+
+    /**
+     * Site korumasi icin panel'deki ilk login islemi
+     */
+    public void loginPanel(){
+        emailBox.sendKeys(ConfigurationReader.get("mainUsername"));
+        passwordBox.sendKeys(ConfigurationReader.get("mainPassword"));
+        loginButton.click();
+
+        Driver.get().findElement(By.xpath("//a[@class='a-close-newsletter']")).click();
+    }
+
+}
