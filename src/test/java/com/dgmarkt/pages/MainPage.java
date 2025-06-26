a410159ddb907edaf0d25390cbabdd48bdbe33ee
Yasin FİREZ <yasinfirez@gmail.com>
2025-06-26 23:38:17 +0300

    feat(mainpage): Alt kategori ve kardeş kategori yönlendirme metotları eklendi
    - Category butonu altındaki Sub-Category sekmelerine yönlendiren metot MainPage.java dosyasına eklendi.
    - Aynı seviyedeki kardeş kategorilere yönlendirme işlemini gerçekleştiren metot MainPage.java dosyasına eklendi.
    - Her iki metot da sayfa nesne modeli (Page Object Model) kapsamında, testlerde tekrar kullanılabilirlik amacıyla tasarlandı.

--- src/test/java/com/dgmarkt/pages/MainPage.java
+++ src/test/java/com/dgmarkt/pages/MainPage.java
@@ -1,12 +1,24 @@
 package com.dgmarkt.pages;
 
+import com.dgmarkt.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class MainPage extends BasePage{
     @FindBy(xpath = "//a[@class='a-close-newsletter']")
     public WebElement popUpButton;
+
     public void closePopUp() {
         popUpButton.click();
     }
+    public void clickMainButton(String buttonName) {
+        Driver.get().findElement(
+                By.xpath("//a[@class='a-top-link']//span[text()='" + buttonName + "']")).click();
+    }
+    public void clickSubButton(String subButtonName) {
+        clickMainButton("Category");
+        Driver.get().findElement(
+                By.xpath("//div[@class='row']/div/h4/a[text()='" + subButtonName + "']")).click();
+        }
 }
