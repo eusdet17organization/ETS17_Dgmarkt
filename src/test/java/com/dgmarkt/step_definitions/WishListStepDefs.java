39005c6c46a237a3dfa2d486790be986f9f3e132
tugrulk <ibrahimmkandemir@gmail.com>
2025-07-05 17:01:21 +0300

    04.07.25 ikan

--- src/test/java/com/dgmarkt/step_definitions/WishListStepDefs.java
+++ src/test/java/com/dgmarkt/step_definitions/WishListStepDefs.java
@@ -2,6 +2,7 @@ package com.dgmarkt.step_definitions;
 
 
 import com.dgmarkt.pages.*;
+import com.dgmarkt.utilities.ConfigurationReader;
 import com.dgmarkt.utilities.Driver;
 import org.junit.Assert;
 import com.dgmarkt.utilities.BrowserUtils;
@@ -9,17 +10,18 @@ import com.dgmarkt.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import static com.dgmarkt.utilities.Driver.driver;
 
 public class WishListStepDefs {
-    CartIconPage cartIconPage =new CartIconPage();
+    CartIconPage cartIconPage = new CartIconPage();
     WistListPage wistListPage = new WistListPage();
-    LoginPanelPage loginPanelPage=new LoginPanelPage();
+    LoginPanelPage loginPanelPage = new LoginPanelPage();
 
-    MainPage mainPage=new MainPage();
+    MainPage mainPage = new MainPage();
 
     LoginPage loginPage = new LoginPage();
 
@@ -32,16 +34,18 @@ public class WishListStepDefs {
         mainPage.clickSubButton("Televisions");
 
     }
+
     @When("User hovers over the product and clicks the wish list button")
     public void user_hovers_over_the_product_and_clicks_the_wish_list_button() {
-     wistListPage.addTheWishListWithHover(11);
+        wistListPage.addTheWishListWithHover(11);
 
 
     }
+
     @Then("User wish list sees added pop-ups")
     public void user_wish_list_sees_added_pop_ups() {
         String actualMessage = wistListPage.getSuccessMessage();
-        String expectedMessage  = wistListPage.getSuccessMessageText();
+        String expectedMessage = wistListPage.getSuccessMessageText();
 
         Assert.assertTrue("Success message is not displayed!", actualMessage.contains(expectedMessage));
 
@@ -49,12 +53,10 @@ public class WishListStepDefs {
     }
 
 
-
-
     @Then("User confirms that the product has been added to the wish list")
     public void user_confirms_that_the_product_has_been_added_to_the_wish_list() {
         BrowserUtils.waitFor(5);
-       wistListPage.clickWishList();
+        wistListPage.clickWishList();
         Assert.assertTrue("Product name is incorrect!",
         wistListPage.getProductName().equalsIgnoreCase("Daewoo D24RTSDVD 24\" Smart LED TV With DVD"));
         System.out.println(" : Product details have been successfully verified:");
@@ -69,6 +71,7 @@ public class WishListStepDefs {
         wistListPage.removeButton.click();
         BrowserUtils.waitFor(1);
     }
+
     @Then("user  {string} sees your message")
     public void user_sees_your_message(String string) {
         Assert.assertTrue(Driver.get().findElement(By.cssSelector("#account-wishlist > div.alert.alert-success.alert-dismissible")).isDisplayed());
@@ -77,12 +80,13 @@ public class WishListStepDefs {
 
     @Then("User adds the product in the wist list to the cart")
     public void user_adds_the_product_in_the_wist_list_to_the_cart() {
-    wistListPage.clickCart.click();
+        wistListPage.clickCart.click();
     }
+
     @Then("User add to cart sees added pop-ups")
     public void user_add_to_cart_sees_added_pop_ups() {
         String actualMessage = wistListPage.getSuccessMessage();
-        String expectedMessage  = wistListPage.getSuccessMessageText();
+        String expectedMessage = wistListPage.getSuccessMessageText();
 
         Assert.assertTrue("Success message is not displayed!", actualMessage.contains(expectedMessage));
 
@@ -94,10 +98,29 @@ public class WishListStepDefs {
     public void user_confirms_that_the_product_has_been_added_to_the_cart() {
         cartIconPage.clickCartIcon();
         Assert.assertTrue("Product name is incorrect!",
-        wistListPage.getProductNameinCartİcon().equalsIgnoreCase("Daewoo D24RTSDVD 24\" Smart LED TV With DVD"));
+                wistListPage.getProductNameinCartİcon().equalsIgnoreCase("Daewoo D24RTSDVD 24\" Smart LED TV With DVD"));
         System.out.println(" : Product details have been successfully verified:");
         System.out.println(" : Name: " + wistListPage.getProductNameinCartİcon());
     }
 
+    @Then("user clicks on category and television")
+    public void user_clicks_on_category_and_television() {
+        mainPage.clickMainButton("Category");
 
+        mainPage.clickSubButton("Televisions");
+    }
+
+    @Then("User wish list sees added pop-ups clicks to login")
+    public void user_wish_list_sees_added_pop_ups_clicks_to_login() {
+       wistListPage.clickLoginLinkWithJS();
+
+
+    }
+
+    @Then("user logs in  clicks to wish list button")
+    public void user_logs_in_clicks_to_wish_list_button() {
+     wistListPage.userLoginWishList();
+
+
+    }
 }
--- src/test/resources/features/WishList.feature
+++ src/test/resources/features/WishList.feature
@@ -28,3 +28,31 @@ Feature: Wist List
      Then User adds the product in the wist list to the cart
      Then User add to cart sees added pop-ups
      Then User confirms that the product has been added to the cart
+
+   Scenario:add wish list products without user login
+     Then  user clicks on category and television
+     When  User hovers over the product and clicks the wish list button
+     Then  User wish list sees added pop-ups clicks to login
+     Then  user logs in  clicks to wish list button
+     Then  User confirms that the product has been added to the wish list
+
+
+   Scenario: wish listen product deletion
+     Then  user clicks on category and television
+     When  User hovers over the product and clicks the wish list button
+     Then  User wish list sees added pop-ups clicks to login
+     Then  user logs in  clicks to wish list button
+     Then  User confirms that the product has been added to the wish list
+     Then user confirms that the base has been deleted
+     Then user  " Success: You have modified your wish list! " sees your message
+  @wip
+  Scenario:wish list add products to cart
+    Then  user clicks on category and television
+    When  User hovers over the product and clicks the wish list button
+    Then  User wish list sees added pop-ups clicks to login
+    Then  user logs in  clicks to wish list button
+    Then  User confirms that the product has been added to the wish list
+    Then User adds the product in the wist list to the cart
+    Then User add to cart sees added pop-ups
+    Then User confirms that the product has been added to the cart
+
